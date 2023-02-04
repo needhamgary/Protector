93028ecba2080bd9fae9a124c1a01d6cae4f3e29
needhamgary <garyneedham1@gmail.com>
2023-02-04 16:46:20 +0000

    add plugins update index

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,2 +1,6 @@
 export * from "./publish.js";
 export * from "./requirePermission.js";
+export * from "./assertFields.js";
+export * from "./confirmation.js";
+export * from "./cooldown.js";
+export * from "./ownerOnly.js";
