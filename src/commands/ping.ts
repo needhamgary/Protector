f632661b0bdae541780a6835ca5004a451892c40
needhamgary <garyneedham1@gmail.com>
2023-02-08 13:04:25 +0000

    refactor: convert ping to Slash only

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,10 +2,9 @@ import { commandModule, CommandType } from '@sern/handler';
 import { publish } from "#plugins";
 
 export default commandModule({
-	type: CommandType.Both,
+	type: CommandType.Slash,
 	plugins: [publish()],
 	description: 'A ping command',
-	//alias : [],
 	execute: async (ctx, args) => {
 		await ctx.reply('Pong 🏓');
 	},
