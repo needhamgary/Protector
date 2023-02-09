8f9ddc194cedee9e470a7a18b9d9e262d3621391
needhamgary <garyneedham1@gmail.com>
2023-02-09 08:30:26 +0000

    feat: command to test verify system

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,11 +1,25 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { slashCommand } from "../helpers/createCommands.js";
 
 export default slashCommand({
-    name: "test",
-    description: "test new command function",
-    plugins: [],
-    async execute(ctx, options) {
-        await ctx.reply({content: "test complete", ephemeral: true})
-    },
-   
-})
+  name: "test",
+  description: "test new command function",
+  plugins: [],
+  async execute(ctx, options) {
+    await ctx.reply({
+      content: "test complete",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder<ButtonBuilder>({
+          components: [
+            new ButtonBuilder({
+              custom_id: "verify-click",
+              style: ButtonStyle.Success,
+              label: "Verify",
+            }),
+          ],
+        }),
+      ],
+    });
+  },
+});
