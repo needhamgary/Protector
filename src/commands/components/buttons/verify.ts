853e2c2253508d756110851bb2aba26c14cf2c2b
needhamgary <garyneedham1@gmail.com>
2023-02-10 06:51:50 +0000

    fix: enable description of button

--- src/commands/components/buttons/verify.ts
+++ src/commands/components/buttons/verify.ts
@@ -10,7 +10,7 @@ import {
 export default commandModule({
   type: CommandType.Button,
   name: "verify-click",
-//   description: "Shows modal to verify.",
+  description: "Shows modal to verify.",
   async execute(button) {
     const verifyModal = new ModalBuilder()
       .setTitle("Verification")
