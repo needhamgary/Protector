a2c98d5b8ef1db878603835bc05ddf37d60e7780
needhamgary <garyneedham1@gmail.com>
2023-02-10 18:57:19 +0000

    fix: update bad import

--- src/commands/components/modals/afk.ts
+++ src/commands/components/modals/afk.ts
@@ -1,26 +1,29 @@
 import { assertFields } from "#plugins";
 import { newModal } from "../../../helpers/createCommands.js";
-import { setAfk } from "../../../mongo/models/requireModels.js";
+import { afk } from "../../../mongo/models/index.js";
 
 export default newModal({
   name: "afk-status",
   description: "sets your afk status",
-  plugins: [assertFields({
-    fields: {
-      reason: /a+n+c/,
-    },
-    failure: (errors, interaction) => {
-      interaction.reply(errors.join("\n"));
-    }
-  })],
+  plugins: [
+    assertFields({
+      fields: {
+        reason: /a+n+c/,
+      },
+      failure: (errors, interaction) => {
+        interaction.reply(errors.join("\n"));
+      },
+    }),
+  ],
   async execute(modal) {
     const { user } = modal;
     const reason = modal.fields.getTextInputValue("reason");
-    
-    const afk = await setAfk(user.id, user.username, reason);
-
-    if (afk) {
-      modal.reply({content: "You are now afk.", ephemeral: true})
-    }
+    afk.create({
+      _id: user.id,
+      username: user.username,
+      date: Date.now(),
+      reason: reason,
+    });
+    return await modal.reply({ content: "You are now afk.", ephemeral: true });
   },
 });
