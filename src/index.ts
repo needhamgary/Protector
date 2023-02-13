a836f30b3b2dbc439cf1d5e3163c97c2706728fa
needhamgary <garyneedham1@gmail.com>
2023-02-13 18:32:24 +0000

    small changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { env } from "#util";
 import { Client, Partials } from "discord.js";
 import {
-	Dependencies,
-	Sern,
-	single,
-	Singleton,
-	ErrorHandling,
-	ModuleStore,
+  Dependencies,
+  Sern,
+  single,
+  Singleton,
+  ErrorHandling,
+  ModuleStore,
 } from "@sern/handler";
 import wait from "node:timers/promises";
 import { createRequire } from "module";
@@ -16,59 +16,42 @@ import pkg from "mongoose";
 
 export let devMode: boolean = false;
 if (process.argv[2] === "--dev") {
-	devMode = true;
-	env;
-	console.clear();
+  devMode = true;
+  env;
+  console.clear();
 } else {
-	env;
+  env;
 }
 
 export const hold = wait.setTimeout;
 export const require = createRequire(import.meta.url);
 
 export const client = new Client({
-	intents: [
-		"DirectMessageReactions",
-		"DirectMessageTyping",
-		"DirectMessages",
-		"GuildBans",
-		"GuildEmojisAndStickers",
-		"GuildIntegrations",
-		"GuildInvites",
-		"GuildMembers",
-		"GuildMessageReactions",
-		"GuildMessageTyping",
-		"GuildMessages",
-		"GuildPresences",
-		"GuildVoiceStates",
-		"GuildWebhooks",
-		"Guilds",
-		"MessageContent",
-	],
-	partials: [
-		Partials.Channel,
-		Partials.GuildMember,
-		Partials.GuildScheduledEvent,
-		Partials.Message,
-		Partials.Reaction,
-		Partials.ThreadMember,
-		Partials.User,
-	],
-	allowedMentions: { repliedUser: false, users: [] },
+  intents: 3246047,
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User,
+  ],
+  allowedMentions: { repliedUser: false, users: [] },
 });
 class ModuleStoreWithLogger extends ModuleStore {
-	constructor(private logger: SparkAdapter) {
-		super();
-	}
+  constructor(private logger: SparkAdapter) {
+    super();
+  }
 }
 //With typescript, you can customize / augment your typings.
 interface MyDependencies extends Dependencies {
-	"@sern/client": Singleton<Client>;
-	"@sern/logger": Singleton<SparkAdapter>;
-	"@sern/errors": Singleton<ErrorHandling>;
-	"@sern/store": Singleton<ModuleStoreWithLogger>;
-	mongoose: Singleton<pkg.Connection>;
-	temps: Singleton<tempVoiceManager>;
+  "@sern/client": Singleton<Client>;
+  "@sern/logger": Singleton<SparkAdapter>;
+  "@sern/errors": Singleton<ErrorHandling>;
+  "@sern/store": Singleton<ModuleStoreWithLogger>;
+  mongoose: Singleton<pkg.Connection>;
+  temps: Singleton<tempVoiceManager>;
 }
 /**
  * Where all of your dependencies are composed.
@@ -80,58 +63,51 @@ interface MyDependencies extends Dependencies {
  * This is used for external event modules as well
  */
 export const useContainer = Sern.makeDependencies<MyDependencies>({
-	build: (root) =>
-		root
-			.add({ "@sern/client": single(() => client) })
-			.upsert({
-				"@sern/logger": single(
-					() => new SparkAdapter("debug", "highlight")
-				),
-			})
-			.add({ process: single(() => process) })
-			.upsert((ctx) => {
-				return {
-					"@sern/store": single(
-						() => new ModuleStoreWithLogger(ctx["@sern/logger"])
-					),
-				};
-			})
-			.add({ mongoose: single(() => pkg.connection) })
-			.add({ temps: single(() => new tempVoiceManager()) }),
+  build: (root) =>
+    root
+      .add({ "@sern/client": single(() => client) })
+      .upsert({
+        "@sern/logger": single(() => new SparkAdapter("debug", "highlight")),
+      })
+      .add({ process: single(() => process) })
+      .upsert((ctx) => {
+        return {
+          "@sern/store": single(
+            () => new ModuleStoreWithLogger(ctx["@sern/logger"])
+          ),
+        };
+      })
+      .add({ mongoose: single(() => pkg.connection) })
+      .add({ temps: single(() => new tempVoiceManager()) }),
 });
 
 //View docs for all options
 
 Sern.init({
-	commands: "dist/commands",
-	events: "dist/events",
-	containerConfig: {
-		get: useContainer,
-	},
+  commands: "dist/commands",
+  events: "dist/events",
+  containerConfig: {
+    get: useContainer,
+  },
 });
 
 (async () => {
-	const [logger] = useContainer("@sern/logger");
-	client.setMaxListeners(0);
-	logger.info(`[Client] Attempting to connect to Discord...`);
-	await hold(500);
+const [logger] = useContainer("@sern/logger");
+client.setMaxListeners(0);
+logger.info(`[Client] Attempting to connect to Discord...`);
+await hold(500);
 
-	if (!devMode) {
-		client.login(env.token!.toString()).catch((e) => {
-			logger.error(
-				`[Client] Unable to connect to Discord.... Error: ${e}`
-			);
-		});
-		await hold(1000);
-		// client.application?.commands.set([])
-	} else {
-		client.login(env.devtoken!.toString()).catch((e) => {
-			logger.error(
-				`[Client] Unable to connect to Discord.... Error: ${e}`
-			);
-		});
-		await hold(1000);
-		client.application?.commands.set([]);
-	}
+if (!devMode) {
+  client.login(env.token!.toString()).catch((e) => {
+    logger.error(`[Client] Unable to connect to Discord.... Error: ${e}`);
+  });
+  await hold(1000);
+} else {
+  client.login(env.devtoken!.toString()).catch((e) => {
+    logger.error(`[Client] Unable to connect to Discord.... Error: ${e}`);
+  });
+  await hold(1000);
+  client.application?.commands.set([]);
+}
 })();
 //client.login(env.token);
